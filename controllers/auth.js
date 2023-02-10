e0c3672de331d088314557fcc7715c307a5d57b0
Irfanasgher <39435767+Irfanasgher@users.noreply.github.com>
2023-02-10 17:09:28 +0500

    sending emails
    sending emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   // let message = req.flash("error");
@@ -87,6 +98,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: "irfanasgher.te@gmail.com",
+            subject: "Signup succeeded!",
+            html: "<h1>You successfully signed up!</h1>",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
     })
     .catch((err) => {
