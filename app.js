5daca800ed2eae2da84f5e3a5118a716516ba127
Irfanasgher <39435767+Irfanasgher@users.noreply.github.com>
2023-03-08 16:11:50 +0500

    E-Commerce App With ReactJS, NodeJs Using File System
    E-Commerce App With ReactJS, NodeJs Using File System

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./models/user");
 
 const app = express();
 
@@ -18,20 +16,9 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("63db6daea97fbb12da04d888")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
-
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(8000);
-});
+app.listen(3300);
